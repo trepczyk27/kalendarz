d8430290fcf5cca4a252e0449ebf48c9eabbd4fe
trepczyk27 <jjd85895@gmail.com>
2022-05-31 15:48:02 +0200

    aaaa

--- app/src/main/java/com/example/kalendarz/MainActivity.java
+++ app/src/main/java/com/example/kalendarz/MainActivity.java
@@ -6,21 +6,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
-        CalendarView calendar;
-        TextView  dataWys;
+    Calendar calendar;
+        CalendarView calendarview;
+        TextView  dataWys,textWys;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calendar = Calendar.getInstance();
 
-        calendar= (CalendarView) findViewById(R.id.calendarView);
+
+        calendar.set(Calendar.MONTH, Calendar.NOVEMBER);
+        calendar.set(Calendar.DAY_OF_MONTH, 9);
+        calendar.set(Calendar.YEAR, 2012);
+
+
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        calendar.add(Calendar.YEAR, 1);
+
+
+
+
+        calendarview= (CalendarView) findViewById(R.id.calendarView);
         dataWys =(TextView) findViewById(R.id.wysDATA);
+        textWys=(TextView) findViewById(R.id.wysTekst);
 
-
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        calendarview.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String Date = (month+1) + "/" + dayOfMonth + "/" + year;
@@ -28,5 +44,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        calendarview.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+
+                String msg = "Selected date Day: " + i2 + " Month : " + (i1 + 1) + " Year " + i;
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
     }
-}
+
+    }
